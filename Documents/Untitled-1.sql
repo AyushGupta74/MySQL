5541e316da8086c4fe8cd754ba8c7fcb95a38ff9
Ayush Gupta <ayush2362000@gmail.com>
2023-10-25 06:31:38 +0530

    updated and completed remaining assignments

--- Documents/Untitled-1.sql
+++ Documents/Untitled-1.sql
@@ -1,13 +1,22 @@
-/*
-Write a function that returns random OTP number of 6 digits.
-*/
-drop function if exists otp;
+/*2.Create two table
+ patient registration table (pid int, pname varchar(20), age int) and 
+ patient prescription table (pid int, pdate date, medicine varchar(40))
+Write a trigger to insert patient prescription details 
+if the entered patient id is present in patient registration table 
+else print “Person is not a registered  patient”.*/
+
+drop trigger if exists Patient;
+drop table if exists patientRegistration ;
+drop table if exists patientPrescription ;
 delimiter $
-create function otp() returns int
-deterministic
+create table patientRegistration(pid int, pname varchar(20), age int);
+create table patientPrescription(pid int, pdate date, medicine varchar(40));
+create trigger Patient before insert on patientPrescription for each row
 begin
-    declare num int;
-    select round(concat(ceil(rand()*10), rand()*1000, rand()*100)) into num;
-    returns num;
+    declare check1 boolean;
+    select false into check1 from patientRegistration where pid= new.pid;
+    if check1 then
+        signal sqlstate '42000' set message_text = "Person is not a registered  patient" ;
+    end if;
 end $
 delimiter ;
